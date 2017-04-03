a706ad8e2246030e46da2cd9cd1349227eaa596a
Aaron Nguyen and Dan Jacob <dan@>
2017-04-03 11:36:08 -0700

    create working form and contructor

---
+++ README.md
@@ -0,0 +1,47 @@
+# _Places I've Been To_
+
+#### _A Page for Places I've Been To April, 3rd 2017_
+
+#### By _**Aaron Nguyen and Dan Jacob**_
+
+## Description
+
+_A project that will provide users information about a place when they click on a picture or name of the place._
+
+## BDD Outline
+
+
+_BDD:
+
+	when user clicks on place it will display stuff about the place:
+
+		input: click on picture or name
+		output: display information about the place
+
+    eg:
+
+  when user enters in a name of a place:
+
+    - form appears that will prompt them for information about the place like 				landmarks
+
+    - this information is appended, then the user can click on it and it will show all of 				the information they entered
+
+## Setup/Installation Requirements
+
+* _View the .html file in any modern browser:
+
+## Known Bugs
+
+_{N/A}_
+
+## Support and contact details
+
+_aaron3831@gmail.com_
+
+## Technologies Used
+
+_HTML, CSS and bootstrap_
+
+### License
+
+Copyright (c) 2017 **_Aaron Nguyen and Dan Jacob_**
---
+++ js/scripts.js
@@ -0,0 +1,59 @@
+//business logic
+function Places(place, landMark, where, timeYear, food, weather) {
+  this.place = place;
+  this.landMark = landMark;
+  this.where = where;
+  this.timeYear = timeYear;
+  this.food = food;
+  this.weather = weather;
+}
+
+Places.prototype.toUppercase = function() {
+  return this.place.toUpperCase();
+  // return this.landMark.toUpperCase();
+  // return this.where.toUpperCase();
+  // return this.timeYear.toUpperCase();
+  // return this.food.toUpperCase();
+  // return this.notes.toUpperCase();
+  // return this.weather.toUpperCase();
+}
+
+//user interface logic
+$(document).ready(function(){
+  $("form#new-place").submit(function(event){
+    event.preventDefault();
+
+    var inputtedPlace = $("input#place").val();
+    var inputtedLandMark = $("input#landMark").val();
+    var inputtedWhere = $("input#where").val();
+    var inputtedTimeYear = $("input#timeYear").val();
+    var inputtedFood = $("input#food").val();
+    var inputtedWeather = $("input#weather").val();
+
+    var newPlace = new Places(inputtedPlace,
+        inputtedLandMark,
+        inputtedWhere,
+        inputtedTimeYear,
+        inputtedFood,
+        inputtedWeather);
+
+    $("ul#placelist").append("<li><span class='placeAppend'>" + newPlace.toUppercase() + "</span></li>");
+
+    $("input#place").val("");
+    $("input#landMark").val("");
+    $("input#where").val("");
+    $("input#timeYear").val("");
+    $("input#food").val("");
+    $("input#weather").val("");
+
+    $(".placeAppend").click(function() {
+      $("#show-place").show();
+      $("#show-place h2").text(newPlace.place);
+      $(".landMarkClass").text(newPlace.landMark);
+      $(".whereClass").text(newPlace.where);
+      $(".timeYearClass").text(newPlace.timeYear);
+      $(".foodClass").text(newPlace.food);
+      $(".weatherClass").text(newPlace.weather);
+    });
+  });
+});
